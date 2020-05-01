505affdecfffa270b95a9653baca982097a762c6
Will Ruskin <willjruskin@gmail.com>
2020-05-01 21:01:05 +0100

    adding files for pod racing

--- PodRace.java
+++ PodRace.java
@@ -4,6 +4,28 @@ import java.util.HashMap;
 
 public class PodRace {
     public static Set<Pod> race(double distance, Set<Pod> racers, double timeSlice, double timeLimit) {
-        // IMPLEMENT THIS METHOD
+        if (distance <= 0){
+          throw new IllegalArguementException("Distance must be greater than 0");
+
+        }
+
+        var winners = new HashSet<Pod>();
+        var time = 0;
+        boolean win_flag = false;
+
+        while (time <= timeLimit){
+          for(var i:racers){
+            var temp = i.distanceTraveled(0,time, 10);
+            if(temp >= distance){
+              winners.add(i)
+              win_flag = true;
+            }
+          }
+          time += timeSlice;
+          if(win_flag = true){
+            return winners;
+          }
+        }
+        return winners;
     }
 }
